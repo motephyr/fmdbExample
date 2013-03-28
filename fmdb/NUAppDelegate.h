47f46ec7a075f8435fa3eba5e975a83835a3ecb4
Mark Wu <mark_wu@ihergo.com>
2013-03-28 16:49:00 +0800

    Initial Commit

---
+++ fmdb/NUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NUAppDelegate.h
+//  fmdb
+//
+//  Created by motephyr on 13/3/28.
+//  Copyright (c) 2013年 motephyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fmdb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fmdb
+//
+//  Created by motephyr on 13/3/28.
+//  Copyright (c) 2013年 motephyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NUAppDelegate class]));
+    }
+}
